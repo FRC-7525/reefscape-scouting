4049eaf8e5bc8c9f72663b1c9a4e02fb981f2c91
saturnaliam <llcsaturnaliam@gmail.com>
2025-01-29 16:12:27 -0600

    exported everything inline

--- app/api/data.ts
+++ app/api/data.ts
@@ -3,7 +3,7 @@ import { MatchData, GamePhase, DRIVER_STATION, START_POSITION, Tag, CLIMB_TYPE }
 
 const path = FileSystem.documentDirectory ?? "" + "matchData.json";
 
-function getMatchData(): Promise<MatchData> {
+export function getMatchData(): Promise<MatchData> {
     return new Promise((resolve, reject) => {
         FileSystem.readAsStringAsync(path)
             .then((res) => {
@@ -12,11 +12,11 @@ function getMatchData(): Promise<MatchData> {
     });
 }
 
-function saveMatchData(newData: MatchData): Promise<void> {
+export function saveMatchData(newData: MatchData): Promise<void> {
     return FileSystem.writeAsStringAsync(path, JSON.stringify(newData));
 }
 
-function updateMatchData(modifier: (matchData: MatchData) => MatchData): Promise<void> {
+export function updateMatchData(modifier: (matchData: MatchData) => MatchData): Promise<void> {
     return new Promise((resolve, reject) => {
         getMatchData()
             .then((matchData) => {
@@ -26,7 +26,7 @@ function updateMatchData(modifier: (matchData: MatchData) => MatchData): Promise
     });
 }
 
-function updateScore(phase: GamePhase, scoreType: "reef" | "algae", newScores: number[]): Promise<void> {
+export function updateScore(phase: GamePhase, scoreType: "reef" | "algae", newScores: number[]): Promise<void> {
     return updateMatchData((matchData) => {
         const newScore = matchData[phase]["score"][scoreType].map((score, i) => score + newScores[i]);
         matchData[phase]["score"][scoreType] = newScore;
@@ -35,78 +35,73 @@ function updateScore(phase: GamePhase, scoreType: "reef" | "algae", newScores: n
     });
 }
 
-function updateReefScore(phase: GamePhase, newScores: number[]): Promise<void> {
+export function updateReefScore(phase: GamePhase, newScores: number[]): Promise<void> {
     return updateScore(phase, "reef", newScores);
 }
 
-function updateAlgaeScore(phase: GamePhase, newScores: number[]): Promise<void> {
+export function updateAlgaeScore(phase: GamePhase, newScores: number[]): Promise<void> {
     return updateScore(phase, "algae", newScores);
 }
 
-function updateLeftLine(leftLine: boolean): Promise<void> {
+export function updateLeftLine(leftLine: boolean): Promise<void> {
     return updateMatchData((matchData) => { 
         matchData["autonomous"]["left_start"] = leftLine;
         return matchData;
     });
 }
 
-function updateName(name: string): Promise<void> {
+export function updateName(name: string): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["scouter_name"] = name;
         return matchData;
     });
 }
 
-function updateNotes(notes: string): Promise<void> {
+export function updateNotes(notes: string): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["notes"] = notes;
         return matchData;
     });
 }
 
-function updateTeamNumber(team: number): Promise<void> {
+export function updateTeamNumber(team: number): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["team_number"] = team;
         return matchData;
     });
 }
 
-function updateMatchNumber(match: number): Promise<void> {
+export function updateMatchNumber(match: number): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["match_number"] = match;
         return matchData;
     });
 }
 
-function updateStationLocation(station: DRIVER_STATION): Promise<void> {
+export function updateStationLocation(station: DRIVER_STATION): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["driver_station"] = station;
         return matchData;
     });
 }
 
-function updateStartLocation(location: START_POSITION): Promise<void> {
+export function updateStartLocation(location: START_POSITION): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["autonomous"]["start_position"] = location;
         return matchData;
     });
 }
 
-function updateTags(tags: Tag[]): Promise<void> {
+export function updateTags(tags: Tag[]): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["tags"] = tags;
         return matchData;
     });
 }
 
-function updateClimb(climb: CLIMB_TYPE): Promise<void> {
+export function updateClimb(climb: CLIMB_TYPE): Promise<void> {
     return updateMatchData((matchData) => {
         matchData["teleop"]["climb"] = climb;
         return matchData;
     });
 }
-
-export { getMatchData, saveMatchData, updateAlgaeScore, updateClimb,
-         updateLeftLine, updateMatchNumber, updateName, updateNotes,
-         updateReefScore, updateStartLocation, updateStationLocation,
-         updateTags, updateTeamNumber };
