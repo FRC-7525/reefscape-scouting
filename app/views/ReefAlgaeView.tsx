d4cbeb55e29a9a62810fe8e6cd1fbfd9de61504b
PramitaUp <pramita0919@gmail.com>
2025-01-29 18:17:37 -0600

    Added new reef, next, back buttons to auto and teleop pages (#33)

--- app/views/ReefAlgaeView.tsx
+++ app/views/ReefAlgaeView.tsx
@@ -4,9 +4,14 @@ import { View } from 'react-native';
 function ReefAlgaeView() {
   return (
     <View>
-      <MathBlock min={0} max={10} />
+      <MathBlock label="L4" min={0} max={10} />
+      <MathBlock label="L3" min={0} max={10} />
+      <MathBlock label="L2" min={0} max={10} />
+      <MathBlock label="L1" min={0} max={10} />
     </View>
+    
   )
+
 }
 
 export default ReefAlgaeView;
