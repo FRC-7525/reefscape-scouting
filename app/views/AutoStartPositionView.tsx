8e6872a625c4ff69af03cc5b409aaa67c05a2f80
saturnaliam <llcsaturnaliam@gmail.com>
2025-02-01 11:06:58 -0600

    fixed indentation, removed Separator, replaced fragments with Views

--- app/views/AutoStartPositionView.tsx
+++ app/views/AutoStartPositionView.tsx
@@ -1,49 +1,37 @@
-import { Switch, StyleSheet, Button, Text, View } from 'react-native';
+import { Switch, Text, View } from 'react-native';
 import { useState } from 'react';
 import React from 'react';
 import RadioButtonComponent from '../components/RadioButtonComponent';
-
-const Separator = () => <View style={styles.separator} />;
+import HorizontalLine from '../components/HorizontalLine';
 
 function AutoStartPositionView () {
-        const [isEnabled, setIsEnabled] = useState(false);
-        const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-        const data = [
-            { label: 'Scoring Table Side', value: 0 },
-            { label: 'Center', value: 1 },
-            { label: 'Audience Side', value: 2 },
-        ];
-    
-        return (
-            <>
-            <Separator></Separator>
+    const [isEnabled, setIsEnabled] = useState(false);
+    const toggleSwitch = () => setIsEnabled(!isEnabled);
+    const data = [
+        { label: 'Scoring Table Side', value: 0 },
+        { label: 'Center', value: 1 },
+        { label: 'Audience Side', value: 2 },
+    ];
 
-                <Text> {'Left start line:'}</Text>
-                <Switch
-                    trackColor={{ false: '#151E26', true: '#E9ECEF' }}
-                    thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-                    ios_backgroundColor=" "
-                    onValueChange={toggleSwitch}
-                    value={isEnabled} /> 
-                <Text> {isEnabled ? 'Yes' : 'No'}</Text>
+    return (
+        <View>
+            <HorizontalLine />
+            <Text>{'Left start line:'}</Text>
+            <Switch
+                trackColor={{ false: '#151E26', true: '#E9ECEF' }}
+                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                ios_backgroundColor=" "
+                onValueChange={toggleSwitch}
+                value={isEnabled} /> 
+            <Text>{isEnabled ? 'Yes' : 'No'}</Text>
 
-                <Text> {'Starting Position:'}</Text>
-                <RadioButtonComponent
-                data= {data}
-                    />
-                <Separator></Separator>
+            <Text> {'Starting Position:'}</Text>
+            <RadioButtonComponent data= {data} />
+            
+            <HorizontalLine />
+        </View>   
+    )
+}
 
-                </>   
-        )
-    }
-
-const styles = StyleSheet.create({
-
-    separator: {
-            marginVertical: 2,
-            borderBottomWidth: StyleSheet.hairlineWidth,
-            },
-    
-        });
 
 export default AutoStartPositionView;
