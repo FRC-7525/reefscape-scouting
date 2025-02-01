61635bb9dcaee3fdbc591d0103ad6a52db781a79
saturnaliam <llcsaturnaliam@gmail.com>
2025-02-01 11:38:27 -0600

    added component for section headings

--- app/views/AlgaeView.tsx
+++ app/views/AlgaeView.tsx
@@ -1,11 +1,13 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import HorizontalLine from "../components/HorizontalLine";
 import MathBlock from "../components/MathBlock";
+import SectionTitle from "../components/SectionTitle";
 
 function AlgaeView() {
     return (
         <View>
             <HorizontalLine />
+            <SectionTitle>Algae</SectionTitle>
 
             <MathBlock min={0} label="In Net" />
             <MathBlock min={0} label="In Processor" />
