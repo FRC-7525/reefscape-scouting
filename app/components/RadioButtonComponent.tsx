f1ed10053459cd8dee45b3170053c0245875e361
saturnaliam <llcsaturnaliam@gmail.com>
2025-02-01 10:57:29 -0600

    fixed indentation and unused imports

--- app/components/RadioButtonComponent.tsx
+++ app/components/RadioButtonComponent.tsx
@@ -1,21 +1,24 @@
-import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
-import {View, Text} from 'react-native'
-import {useState} from 'react'
+import RadioForm from 'react-native-simple-radio-button';
+import { View } from 'react-native'
+import { useState } from 'react'
 
 interface RadioButtonComponentProps {
-    data: { label: string; value: number }[];
+    data: {
+        label: string;
+        value: number
+    }[];
 }
 
 function RadioButtonComponent({ data }: RadioButtonComponentProps) {
     const [value, setValue] = useState<number>(data[0]?.value || 0);
-        return (
-            <View>
+    return (
+        <View>
             <RadioForm
                 radio_props={data}
                 initial={value}
-                onPress={(value) => setValue(value)}
-            />
-            </View>
-        );
-    } 
+                onPress={(value) => setValue(value)} />
+        </View>
+    );
+} 
+
 export default RadioButtonComponent;
