b8d4a013d1a86cbecaf5eb6e6c1987a8800d8270
saturnaliam <llcsaturnaliam@gmail.com>
2025-02-03 15:55:50 -0600

    styled top section of auto page

--- app/views/AutoStartPositionView.tsx
+++ app/views/AutoStartPositionView.tsx
@@ -1,7 +1,6 @@
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { useState } from 'react';
 import { Switch } from 'react-native-paper';
-import React from 'react';
 import RadioButtonComponent from '../components/RadioButtonComponent';
 import HorizontalLine from '../components/HorizontalLine';
 
@@ -10,23 +9,31 @@ function AutoStartPositionView () {
     const toggleSwitch = () => setIsEnabled(!isEnabled);
 
     return (
-        <View>
-
-            <Text>{'Left start line:'}</Text>
-            <Switch
-                trackColor={{ false: '#151E26', true: '#E9ECEF' }}
-                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-                onValueChange={toggleSwitch}
-                value={isEnabled} /> 
-            <Text>{isEnabled ? 'Yes' : 'No'}</Text>
-
-            <Text> {'Starting Position:'}</Text>
-            <RadioButtonComponent data={["Scoring Table Side", "Center", "Audience Side"]} />
-            
-            <HorizontalLine />
+        <View style={styles.container}>
+            <View style={styles.side}>
+                <Text>{'Left start line:'}</Text>
+                <Switch
+                    color="#2c2c2c"
+                    onValueChange={toggleSwitch}
+                    value={isEnabled} /> 
+            </View>
+            <View style={[{ width: 0.5, backgroundColor: "black" }]}/>
+            <View style={styles.side}>
+                <Text> {'Starting Position:'}</Text>
+                <RadioButtonComponent data={["Scoring Table Side", "Center", "Audience Side"]} />
+            </View>
         </View>   
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+    },
+    side: {
+        flex: 1,
+        alignItems: 'center'
+    }
+});
 
 export default AutoStartPositionView;
