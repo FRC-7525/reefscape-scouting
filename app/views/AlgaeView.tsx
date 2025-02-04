8a95b8ad0ab212e11c37221ea624cc531bbeac05
saturnaliam <llcsaturnaliam@gmail.com>
2025-02-04 14:18:51 -0600

    alge view now persists :]

--- app/views/AlgaeView.tsx
+++ app/views/AlgaeView.tsx
@@ -2,13 +2,17 @@ import { View, Text } from "react-native";
 import HorizontalLine from "../components/HorizontalLine";
 import MathBlock from "../components/MathBlock";
 import SectionTitle from "../components/SectionTitle";
-import { updateAlgaeScore } from "../api/data";
+import { getMatchData, updateAlgaeScore } from "../api/data";
 
 interface AlgaeViewProps {
     phase: "teleop" | "autonomous";
 }
 
 function AlgaeView({ phase }: AlgaeViewProps) {
+    const getOldCount = (location: "net" | "processor") => {
+        return getMatchData().then((data) => data[phase]["algae"][location]);
+    }
+
     return (
         <View>
             <HorizontalLine />
@@ -16,10 +20,10 @@ function AlgaeView({ phase }: AlgaeViewProps) {
 
             <MathBlock min={0} label="In Net" onPress={(count: number) => {
                 updateAlgaeScore(phase, "net", count);
-            }} />
+            }} oldCount={getOldCount("net")} />
             <MathBlock min={0} label="In Processor" onPress={(count: number) => {
                 updateAlgaeScore(phase, "processor", count);
-            }} />
+            }} oldCount={getOldCount("processor")} />
         </View>
     )
 }
