6d144fce194d22e453d2dad59485303533fc74c8
PramitaUp <pramita0919@gmail.com>
2025-02-05 17:12:29 -0600

    Styled Endgame View (#44)
    * Styled Endgame View
    
    * Fixed Climb label

--- app/views/EndgameView.tsx
+++ app/views/EndgameView.tsx
@@ -1,23 +1,39 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import React from "react";
 import RadioButtonComponent from "../components/RadioButton";
 import { Divider } from "react-native-paper";
 import { getMatchData, updateClimb } from "../api/data";
 import { CLIMB_TYPE } from "../api/data_types";
-
+import SectionTitle from "../components/SectionTitle";
 
 function EndgameView() {
     return (
         <View>
-            <Text>{"Climb?:"}</Text>
-            <RadioButtonComponent data={["Deep Climb", "Shallow Climb", "Park", "Nothing"]}
-                onSelect={(option: string) => {
-                    updateClimb(option as CLIMB_TYPE);
-                }}
-                oldSelected={getMatchData().then((data) => data["teleop"]["climb"])}/>
             <Divider />
+            <SectionTitle>{"Climb"}</SectionTitle>
+           
+            <View style={styles.container}>
+                <View style={styles.centerRadioButtons}>
+                    <RadioButtonComponent data={["Deep Climb", "Shallow Climb", "Park", "Nothing"]}
+                        onSelect={(option: string) => {
+                            updateClimb(option as CLIMB_TYPE);
+                        }}
+                        oldSelected={getMatchData().then((data) => data["teleop"]["climb"])}/>
+                </View>
+            </View>
         </View>
     );
+
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+    },
+    centerRadioButtons: {
+        flex: 1,
+        alignItems: 'center'
+    }
+})
 
 export default EndgameView;
