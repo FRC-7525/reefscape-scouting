96f9c74b23678ae5a4d9fca9871971860a7b1aae
saturnaliam <llcsaturnaliam@gmail.com>
2025-02-07 11:17:34 -0600

    added requirements to index page before being able to continue

--- app/index.tsx
+++ app/index.tsx
@@ -5,15 +5,32 @@ import PageHeader from './components/Header';
 import LabeledTextInput from './components/LabeledTextInput';
 import { getMatchData, updateMatchNumber, updateName, updateTeamNumber, updateDriverStation } from './api/data';
 import { DRIVER_STATION } from './api/data_types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import RadioButton from './components/RadioButton';
 
 export default function App() {
+    const [ nameFilled, setNameFilled ] = useState(false);
+    const [ teamNumberFilled, setTeamNumberFilled ] = useState(false);
+    const [ matchFilled, setMatchFilled ] = useState(false);
+
+    useEffect(() => {
+        const initializeRequirements = () => {
+            getMatchData().then((data) => {
+                setNameFilled(data["scouterName"] !== "");
+                setTeamNumberFilled(data["teamNumber"] !== 0);
+                setMatchFilled(data["matchNumber"] !== 0);
+            })
+        }
+
+        initializeRequirements();
+    })
+
     return (
         <View style={styles.container}>
             <PageHeader title='Main' pageNumber='1/4' />
             <LabeledTextInput placeholder="Name" editable={true} submit={(e) => {
                 updateName(e.nativeEvent.text);
+                setNameFilled(e.nativeEvent.text !== "");
             }} oldValue={
                 getMatchData().then((data) => data["scouterName"])
             } />
@@ -21,6 +38,7 @@ export default function App() {
             <LabeledTextInput placeholder="Team number" editable={true}
                 inputMode='numeric' submit={(e) => {
                     updateTeamNumber(Number(e.nativeEvent.text));
+                    setTeamNumberFilled(e.nativeEvent.text !== "" && e.nativeEvent.text !== "0");
                 }} oldValue={
                     getMatchData().then((data) => data["teamNumber"].toString())
                 } />
@@ -28,6 +46,7 @@ export default function App() {
             <LabeledTextInput placeholder="Match number" editable={true}
                 inputMode='numeric' submit={(e) => {
                     updateMatchNumber(Number(e.nativeEvent.text));
+                    setMatchFilled(e.nativeEvent.text !== "" && e.nativeEvent.text !== "0");
                 }} oldValue={
                     getMatchData().then((data) => data["matchNumber"].toString())
                 } />
@@ -37,7 +56,8 @@ export default function App() {
                 onSelect={(selected: string) => { updateDriverStation(selected as DRIVER_STATION) }}
                 oldSelected={getMatchData().then((data) => data["driverStation"])} />
             
-            <NavButton text="Go" pageName="auto" />
+            <NavButton text="Go" pageName="auto"
+                disabled={ !(nameFilled && teamNumberFilled && matchFilled) } />
 
             <StatusBar style="auto" />
         </View>
