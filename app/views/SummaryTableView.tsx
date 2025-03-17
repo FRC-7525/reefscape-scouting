e07a4b34c927113b3fa1c14fbd20660dcc904b4d
nanobot567 <567.nanobot@gmail.com>
2025-03-17 16:15:20 -0500

    booleans in summary are now yes/no

--- app/views/SummaryTableView.tsx
+++ app/views/SummaryTableView.tsx
@@ -4,15 +4,28 @@ import { View } from "react-native";
 import { getMatchData } from "../api/data";
 import TableRow from "../components/TableRow";
 
+// makes data point human friendly (true/false -> yes/no)
+function makeHumanFriendly(data: any) {
+    data = data.toString();
+
+    if (data == "true") {
+        data = "Yes";
+    } else if (data == "false") {
+        data = "No";
+    }
+
+    return data;
+}
+
 function SummaryTableView() {
     return (
         <View>
-             <DataTable>
-                <TableHeader titles={[ "Match Summary" ]} />
-                <TableRow label="Scouter" data={ getMatchData().then((data) => data["scouterName"]) } />
+            <DataTable>
+                <TableHeader titles={["Match Summary"]} />
+                <TableRow label="Scouter" data={getMatchData().then((data) => data["scouterName"])} />
                 <TableRow label="Match Number" data={getMatchData().then((data) => data["matchNumber"].toString())} />
                 <TableRow label="Start Position" data={getMatchData().then((data) => data["autonomous"]["startPosition"])} />
-                <TableRow label="Left Start" data={getMatchData().then((data) => data["autonomous"]["leftStart"].toString())} />
+                <TableRow label="Left Start" data={getMatchData().then((data) => makeHumanFriendly(data["autonomous"]["leftStart"]))} />
                 <TableRow label="Climb Type" data={getMatchData().then((data) => data["teleop"]["climb"].toString())} />
             </DataTable>
         </View>
